67dbab9e8a475f6384381d5e489a3f2906307661
mdeliso97 <marco.deliso@students.unibe.ch>
2023-10-02 11:28:25 +0200

    commented part which is not working because of not working installed package crypto

--- Exercise 1/ECB.py
+++ Exercise 1/ECB.py
@@ -1,24 +1,24 @@
 import json
 from base64 import b64encode
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
-from Crypto.Random import get_random_bytes
+#from Crypto.Cipher import AES
+#from Crypto.Util.Padding import pad
+#from Crypto.Random import get_random_bytes
 
 
 def ecb(label):
-    # Key and plaintext
-    key = get_random_bytes(16)  # 128-bit key
-    plaintext = b"This is the plaintext message"
-
-    # Create an AES cipher object in ECB mode
-    cipher = AES.new(key, AES.MODE_ECB)
-
-    # Encryption
-    padded_plaintext = pad(plaintext, AES.block_size)
-    ciphertext = cipher.encrypt(padded_plaintext)
-
-    # Decryption
-    decipher = AES.new(key, AES.MODE_ECB)
-    decrypted_data = unpad(decipher.decrypt(ciphertext), AES.block_size)
+    # # Key and plaintext
+    # key = get_random_bytes(16)  # 128-bit key
+    # plaintext = b"This is the plaintext message"
+    # 
+    # # Create an AES cipher object in ECB mode
+    # cipher = AES.new(key, AES.MODE_ECB)
+    #
+    # # Encryption
+    # padded_plaintext = pad(plaintext, AES.block_size)
+    # ciphertext = cipher.encrypt(padded_plaintext)
+    #
+    # # Decryption
+    # decipher = AES.new(key, AES.MODE_ECB)
+    # decrypted_data = unpad(decipher.decrypt(ciphertext), AES.block_size)
 
     return label.config(text="Button ecb!")
