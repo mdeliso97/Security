da1dfd6f4316d278656ab5f0bc5c7b2da7463fc1
mdeliso97 <marco.deliso@students.unibe.ch>
2023-11-05 21:54:41 +0100

    Linux installer should also work with the changes provided, needs to be tested

--- installer_Linux.sh
+++ installer_Linux.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+parent_dir="$(pwd)"
+
+python_version="3.9.13"
+
+python_path="$parent_dir/Utilities"
+
+cd $python_path
+
+echo "extract the source code..."
+
+tar -xvf Python-$python_version.tgz
+
+echo "configure, build, and install Python"
+
+./configure --prefix=$python_path
+
+make
+
+sudo make install
+
+echo "completed installation of Python $python_version"
+
+echo "clean up..."
+
+cd $python_path
+
+rm -rf Python-$python_version
+
+rm -f Python-$python_version.tgz
+
+rm python-$python_version-amd64.exe
+
+cd $parent_dir
+
 echo "Initializing virtual environment..."
 
 python -m venv crypto_env
