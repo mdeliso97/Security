0d425d9a217e65be2337f1ab7536b7883d6c9557
mdeliso97 <marco.deliso@students.unibe.ch>
2023-11-05 21:26:19 +0100

    Added packages for specific python versions for auto install, still wrking on the bash file

--- installer_Windows.bat
+++ installer_Windows.bat
@@ -1,5 +1,39 @@
 @echo off
 
+parent_dir="$(pwd)"
+
+python_version="3.9.13"
+
+python_path="$parent_dir/Utilities"
+
+cd Utilities/Python-$python_version
+
+echo Extract the source code...
+
+tar -xvf Python-$python_version.tgz
+
+echo Configure, build, and install Python
+
+./configure --prefix=$python_path
+
+make
+
+sudo make install
+
+echo completed installation of Python $python_version
+
+echo clean up
+
+cd ..
+
+rm -rf Python-$python_version
+
+rm Python-$python_version.tgz
+
+cd $parent_dir
+
+
+
 echo Initializing virtual environment...
 
 python -m venv crypto_env
@@ -23,6 +57,7 @@ REM Create a new batch file
   echo @echo off
   echo echo Activating virtual environment...
   echo call crypto_env\Scripts\activate.bat
+  echo export PATH="$python_path:$PATH"
   echo echo Activation complete.
   echo echo Start application...
   echo python cryptology.py
